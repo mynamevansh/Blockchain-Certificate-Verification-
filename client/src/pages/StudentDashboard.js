37f249c31e4573a241aa59f3dc7ed965fb00dc9c
iamvansh <vanshranawat48@gmail.com>
2025-10-21 21:08:34 +0530

    remove usercallback

--- client/src/pages/StudentDashboard.js
+++ client/src/pages/StudentDashboard.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import { useLoading } from '../context/LoadingContext';
